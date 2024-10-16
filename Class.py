767872937a51241106a00c65dd3e3f6dc1be7b80
Aleem Khan <aleemkirk@gmail.com>
2024-10-15 20:28:04 -0400

    add error correction and data pre-processing to data input

--- Class.py
+++ Class.py
@@ -9,18 +9,40 @@ class PortfolioOptimizer:
         self.__end_date = end_date
         self.portfolio = self.securities + [self.__market]
         self.__path = 'data/'
+        self.num_securities = len(securities)
 
     #read securities data into a DataFrame
     def getData(self) -> pd.DataFrame:
+
         self.data = pd.DataFrame(columns=['date'])
+        try:
+            for ticker in self.portfolio:
+                df = pd.read_csv(self.__path+ticker+'.csv')
+                self.data = pd.merge(self.data, df[['date', 'close']], on='date', how='outer', suffixes=(ticker, ticker))
+        except:
+            print(f"Invalid security ticker given: {ticker}")
+            self.data = []
+            return
+        else:
+            #rename column names with Ticker values
+            self.data.columns = ['date'] + [i for i in self.portfolio]
 
-        for ticker in self.portfolio:
-            data = pd.read_csv(self.__path+ticker+'.csv')
-            self.data = pd.merge(self.data, data[['date', 'close']], on='date', how='outer', suffixes=(ticker, ticker))
+            #convert dates to datetime
+            self.data['date'] = pd.to_datetime(self.data['date'])
 
-        #rename column names with Ticker values
-        self.data.columns = ['date'] + [i for i in self.portfolio]
+            #sort dates
+            self.data = self.data.sort_values(by='date')
 
-        return self.data
+            # Reset the index
+            self.data.reset_index(drop=True, inplace=True)
+
+            #drop any rows with NaNs
+            #self.data = self.data.dropna()
+
+            #filter data from start and end dates
+            self.data = self.data[(self.data['date'] >= self.__start_date) & (self.data['date'] <= self.__end_date)]
+
+
+        return self.data 
 
 
