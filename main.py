c67a2d631c8032943ff8d211fb03c83136bcee7f
Aleem Khan <aleemkirk@gmail.com>
2024-10-20 17:00:45 -0400

    add manual weight parsing

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     parser = argparse.ArgumentParser(description='Add weights')
     parser.add_argument('-p', '--path', required=True,
                         type=str, help='Path to security tickers')
+    parser.add_argument('-m', '--manual', required=False,
+                        type=str, default='n', help='Enter investment weights manually if True')
     parser.add_argument('-w', '--weights', nargs='+',  required=False,
                         type=int, default=None, help='Indicate investment weights in order')
     parser.add_argument('-n', '--num',  required=True,
@@ -15,15 +17,22 @@ def main():
 
     args = parser.parse_args()
 
+    man_weights = args.manual
     path = args.path
-    weights = args.weights
     clusters = args.num
+    data = pd.read_csv(path)
+    securities = data['TICKERS'].tolist()
 
-    securities = pd.read_csv(path)['TICKERS'].tolist()
+    if man_weights.lower() == 'y':
+        weights = args.weights
+    elif man_weights.lower() == 'n':
+        weights = data['WEIGHTS'].tolist()
+    else:
+        raise Exception('enter y/n for manual flag.')
 
     d = PortfolioDiversifier(securities=securities,
                              weights=weights, clusters=clusters)
-    
+
     print('\nDefault Portfolio Metrics:')
     d.diversify()
     d.stockClusters()
