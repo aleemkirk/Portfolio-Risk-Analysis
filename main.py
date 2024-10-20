95c0d6af13006b45056c9fdf3328f8765d5e6a02
Aleem Khan <aleemkirk@gmail.com>
2024-10-20 16:43:06 -0400

    add main() function call to accept user input

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from Class import PortfolioMetrics, PortfolioDiversifier
+from Class import PortfolioDiversifier
 import pandas as pd
-import numpy as np
+import argparse
 
 
-w = [.1, .5, .2, .2]
-x = ['AAOI', 'TSLA', 'MSFT', 'NVDA', 'AMZN', 'META',
-     'CTSH', 'CRWD', 'TXN', 'GOOGL', 'DASH', 'MDB']
-d = PortfolioDiversifier(securities=x, clusters=6)
-d.diversify()
-d.stockClusters()
-d.divPortfolio()
+def main():
+
+    parser = argparse.ArgumentParser(description='Add weights')
+    parser.add_argument('-p', '--path', required=True,
+                        type=str, help='Path to security tickers')
+    parser.add_argument('-w', '--weights', nargs='+',  required=False,
+                        type=int, default=None, help='Indicate investment weights in order')
+    parser.add_argument('-n', '--num',  required=True,
+                        type=int, help='Desired portfolio size')
+
+    args = parser.parse_args()
+
+    path = args.path
+    weights = args.weights
+    clusters = args.num
+
+    securities = pd.read_csv(path)['TICKERS'].tolist()
+
+    d = PortfolioDiversifier(securities=securities,
+                             weights=weights, clusters=clusters)
+    
+    print('\nDefault Portfolio Metrics:')
+    d.diversify()
+    d.stockClusters()
+    d.divPortfolio()
+
+
+if __name__ == '__main__':
+    main()
